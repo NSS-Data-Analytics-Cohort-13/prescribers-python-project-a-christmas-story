fcccee407709d6680b3fe621fd8d11f46560cef9
tosabrina <sabrina.to99@gmail.com>
2024-12-12 21:46:05 -0600

    Q1_Query_Started

---
+++ prescribers_sql_queries.sql
@@ -0,0 +1,40 @@
+-- * Which Tennessee counties had a disproportionately high number of opioid prescriptions?
+
+--MIKE'S QUERY:--
+select 
+	fc.county as county
+,	d.opioid_drug_flag
+from drug as d
+cross join fips_county as fc
+where d.opioid_drug_flag = 'Y' and fc.state = 'TN'
+group by d.opioid_drug_flag, fc.county
+
+--MY QUERY:--
+SELECT 
+    f.fipscounty
+,	f.county
+FROM 
+    prescription AS pres
+INNER JOIN 
+    drug AS d 
+	ON pres.drug_name = d.drug_name
+INNER JOIN 
+    prescriber AS pr 
+	ON pres.npi = pr.npi
+INNER JOIN 
+    zip_fips AS z 
+	ON pr.nppes_provider_zip5 = z.zip
+INNER JOIN 
+    fips_county AS f 
+	ON z.fipscounty = f.fipscounty
+INNER JOIN 
+    population AS pop 
+	ON f.fipscounty = pop.fipscounty
+WHERE 
+    d.opioid_drug_flag = 'Y'
+    AND f.state = 'TN'
+
+-- * Who are the top opioid prescibers for the state of Tennessee?
+-- * What did the trend in overdose deaths due to opioids look like in Tennessee from 2015 to 2018?
+-- * Is there an association between rates of opioid prescriptions and overdose deaths by county?
+-- * Is there any association between a particular type of opioid and number of overdose deaths?

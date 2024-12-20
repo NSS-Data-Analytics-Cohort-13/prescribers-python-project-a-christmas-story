09253b19670643bed5af20bf92de18fd37ac8bf0
tacaelest <tesscael@gmail.com>
2024-12-19 21:29:55 -0600

    post class push 12/19/24

--- notebooks/SQL Code Group Work.sql
+++ notebooks/SQL Code Group Work.sql
@@ -70,6 +70,7 @@ INNER JOIN cte2
 
 --Q2 : Who are the top opioid prescibers for the state of Tennessee?
 -- prescibers, filter for TN, total day supply and/or 30 day supply
+--Michelle's Query
 SELECT COUNT(opioid_drug_flag),pres.nppes_provider_last_org_name
 FROM prescriber as pres
 INNER JOIN prescription as p
@@ -89,40 +90,97 @@ GROUP BY
 pres.nppes_provider_last_org_name, d.opioid_drug_flag
 ORDER BY COUNT(opioid_drug_flag) DESC
 
---Q3 : What did the trend in overdose deaths due to opioids look like in Tennessee from 2015 to 2018?
--- deaths, filter TN, filter 2015 to 2018
-SELECT COUNT(o.overdose_deaths) AS deaths
-FROM
-    prescription AS p
-INNER JOIN
-    drug AS d
-ON
-    p.drug_name = d.drug_name
-INNER JOIN
-    prescriber AS pres
-ON
-    pres.npi = p.npi
+--Sabrina's Query
+SELECT 
+	COUNT(opioid_drug_flag) AS opioid_flag_count
+,	CONCAT(pres.nppes_provider_first_name,' ',pres.nppes_provider_last_org_name) AS provider_name
+FROM prescriber as pres
+INNER JOIN prescription as p
+ON pres.npi=p.npi
+INNER JOIN drug as d
+ON p.drug_name=d.drug_name
 INNER JOIN
     zip_fips AS z
-ON
-    pres.nppes_provider_zip5 = z.zip
-INNER JOIN
-    overdose_deaths AS o
-ON
-    CAST(z.fipscounty AS INTEGER) = CAST(o.fipscounty AS INTEGER)
+	ON pres.nppes_provider_zip5 = z.zip
 INNER JOIN
     fips_county AS f
-ON
-    CAST(o.fipscounty AS INTEGER) = CAST(f.fipscounty AS INTEGER)
+	ON z.fipscounty = f.fipscounty
 WHERE
     d.opioid_drug_flag = 'Y'
     AND f.state = 'TN'
-    AND o.year BETWEEN 2015 AND 2018;
+GROUP BY
+	d.opioid_drug_flag
+,	provider_name
+ORDER BY 
+	opioid_flag_count DESC
+
+--Mike's Query/Group's Official Answer: 
+
+SELECT 
+	CONCAT(nppes_provider_first_name, ' ', nppes_provider_last_org_name) as provider_name, 
+	nppes_provider_zip5 as provider_zip,
+	COUNT(d.opioid_drug_flag) as opioid_count
+FROM prescriber scribe
+JOIN prescription script
+ON scribe.npi = script.npi
+JOIN drug d
+ON script.drug_name = d.drug_name
+WHERE  d.opioid_drug_flag = 'Y' AND nppes_provider_state = 'TN'
+GROUP BY provider_name, provider_zip
+ORDER BY opioid_count DESC
+
+--Group Answer:
+
+
+--Q3 : What did the trend in overdose deaths due to opioids look like in Tennessee from 2015 to 2018?
+-- deaths, filter TN, filter 2015 to 2018
+
+-- if overdose_deaths includes deaths from other drug types
+SELECT SUM(od.overdose_deaths), od.year
+	FROM overdose_deaths od
+	JOIN fips_county fc
+	ON od.fipscounty = fc.fipscounty::integer
+	JOIN zip_fips zip
+	ON fc.fipscounty = zip.fipscounty
+	JOIN prescriber scribe
+	ON scribe.nppes_provider_zip5 = zip.zip
+	JOIN prescription script
+	ON scribe.npi = script.npi
+	JOIN drug d
+	ON script.drug_name = d.drug_name
+WHERE d.opioid_drug_flag = 'Y' AND fc.state = 'TN' AND od.year BETWEEN 2015 AND 2019
+GROUP BY od.year
+
+--Q3 overdose_deaths only pertains to opioid ods
+SELECT SUM(od.overdose_deaths), od.year
+	FROM overdose_deaths od
+	JOIN fips_county fc
+	ON od.fipscounty = fc.fipscounty::integer
+	JOIN zip_fips zip
+	ON fc.fipscounty = zip.fipscounty
+WHERE fc.state = 'TN' AND od.year BETWEEN 2015 AND 2019
+GROUP BY od.year
 
 --Q4 : Is there an association between rates of opioid prescriptions and overdose deaths by county?
 -- prescriptions, deaths, filter by county, compare
 
+--Plot as two different variables in Python 
 
 --Q5 : Is there any association between a particular type of opioid and number of overdose deaths?
 -- opioid type, deaths, compare
 
+SELECT SUM(o.overdose_deaths) AS sum_od, o.year, d.drug_name
+	FROM prescription AS rx
+	JOIN drug AS d
+	ON rx.drug_name=d.drug_name
+	JOIN prescriber AS p
+	ON rx.npi=p.npi
+	JOIN zip_fips AS zip
+	ON p.nppes_provider_zip5=zip.zip
+	JOIN fips_county AS fc
+	ON zip.fipscounty::integer=fc.fipscounty::integer
+	JOIN overdose_deaths AS o
+	ON fc.fipscounty::Integer=o.fipscounty::integer
+WHERE fc.state = 'TN' AND d.opioid_drug_flag = 'Y'
+GROUP BY o.year, d.drug_name
+ORDER BY o.year
